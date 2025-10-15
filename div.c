1a88de440ec86ab6313e5a482188ba03148adc2b
Sujay Chandake <chandakesujay@gmail.com>
2025-10-15 21:13:19 +0530

    Update div.c

--- div.c
+++ div.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Function to divide two numbers
-float divide(float num1, float num2) {
+float div(float num1, float num2) {
     if (num2 == 0) {
         printf("Error! Division by zero.\n");
         return 0;
